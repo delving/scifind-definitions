de3f5a66f09c805b5900b6ab412f1bf7d9cf3c1b
Sjoerd Janssen <sjoerdjanssenen@gmail.com>
2023-03-16 13:12:39 +0100

    update Timelinecontroller

--- scifind-api/app/Http/Controllers/Api/TimelineController.php
+++ scifind-api/app/Http/Controllers/Api/TimelineController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
-use App\Services\Scifind\V1\Object as ScifindObject;
-use App\Services\Scifind\V1\ObjectServiceClient;
+use App\Services\Scifind\V1\GetTimelineRequest;
+use App\Services\Scifind\V1\TimelineServiceClient;
 
 class TimelineController extends Controller
 {
     public function getTimeline(): JsonResponse
     {
-
+        $client = new TimelineServiceClient();
+        $request = new GetTimelineRequest();
+        $request->setId(1);
+        dd($client->GetObject($request));
     }
 }
